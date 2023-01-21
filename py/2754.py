2d7ad8b45dedabdf28733e9f7718fc21706b0c7f
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-01-21 10:47:55 +0900

    2754

---
+++ py/2754.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = str(input())
+if n[0]=='A':
+  if n[1] == '+':print("4.3")
+  elif n[1] =='0':print("4.0")
+  elif n[1] =='-':print('3.7')
+elif n[0] =='B':
+  if n[1]=='+':print('3.3')
+  elif n[1]=='0':print('3.0')
+  elif n[1]=='-':print('2.7')
+elif n[0]=='C':
+  if n[1]=='+':print('2.3')
+  elif n[1]=='0':print('2.0')
+  elif n[1]=='-':print('1.7')
+elif n[0] =='D':
+  if n[1]=='+':print('1.3')
+  elif n[1]=='0':print('1.0')
+  elif n[1]=='-':print('0.7')
+else: print('0.0')
